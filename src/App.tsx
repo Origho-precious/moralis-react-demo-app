561f13adb0b2553572d6cb50d2842e8a00f00aee
Origho-precious <Origho9@gmail.com>
2022-01-30 00:09:55 +0100

    feat: rendering app with all funcs

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import { useMoralis } from "react-moralis";
+import { useState } from "react";
+import Form from "./components/Form/Form";
+import Modal from "./components/Modal/Modal";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const { authenticate, isAuthenticated, user, logout, isAuthenticating } =
+		useMoralis();
+	const [showModal, setShowModal] = useState(false);
+
+	return (
+		<StyledDiv className="font-sans">
+			<section>
+				{isAuthenticating ? (
+					<p>Loading...</p>
+				) : isAuthenticated ? (
+					<button type="button" onClick={async () => await logout()}>
+						Logout
+					</button>
+				) : (
+					<>
+						<button type="button" onClick={async () => await authenticate()}>
+							Connect to wallet
+						</button>
+						<button type="button" onClick={() => setShowModal(true)}>
+							Auth with Email
+						</button>
+					</>
+				)}
+			</section>
+
+			<h2>React-Moralis Demo App</h2>
+			<ul>
+				<li>
+					<h4>User Details</h4>
+					<ul>
+						<li>
+							Wallet Address: {user?.attributes?.authData?.moralisEth?.id}
+						</li>
+					</ul>
+				</li>
+			</ul>
+
+			<Modal showModal={showModal} onModalClose={() => setShowModal(false)}>
+				<Form onModalClose={() => setShowModal(false)} />
+			</Modal>
+		</StyledDiv>
+	);
+};
+
+const StyledDiv = styled.div`
+	width: 90%;
+	margin: 2rem auto;
+
+	& h2 {
+		text-align: center;
+	}
+
+	& section {
+		display: flex;
+		width: max-content;
+		margin-left: auto;
+
+		& button {
+			display: block;
+			border: none;
+			outline: none;
+			background: #000;
+			color: #fff;
+			height: 40px;
+			width: max-content;
+			padding: 0 1rem;
+			border-radius: 5px;
+			margin: 0 0.8rem;
+			cursor: pointer;
+		}
+	}
+`;
 
 export default App;
