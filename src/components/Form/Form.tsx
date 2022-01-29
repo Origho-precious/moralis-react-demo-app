764e3625c41f5c9073ecda0728652283691b7a8f
Origho-precious <Origho9@gmail.com>
2022-01-30 00:09:21 +0100

    feat: finalizing Signup and Login logic

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -2,28 +2,36 @@ import { useState } from "react";
 import { useMoralis } from "react-moralis";
 
 interface FormProps {
-	type: "login" | "signup";
+	onModalClose: () => void;
 }
 
-const Form: React.FC<FormProps> = ({ type }) => {
-	const { isAuthenticating, signup, login } = useMoralis();
+const Form: React.FC<FormProps> = ({ onModalClose }) => {
+	const { isAuthenticating, signup, login, authError, hasAuthError } =
+		useMoralis();
 	const [email, setEmail] = useState("");
 	const [username, setUsername] = useState("");
 	const [password, setPassword] = useState("");
-	const [error, setError] = useState(false);
+	const [error, setError] = useState("");
+	const [type, setType] = useState<"login" | "signup">("signup");
 
-	const submitHandler = async (e: any) => {
+	const submitHandler = (e: any) => {
 		e.preventDefault();
 
-		if (password.length < 8) {
-			return setError(true);
-		}
+		try {
+			if (password.length < 8) {
+				return setError("Password must contain 8 characters or more!");
+			}
 
-		if (type === "signup") {
-			return await signup(username, password, email);
-		}
+			if (type === "signup") {
+				signup(username, password, email);
+				return !hasAuthError && onModalClose();
+			}
 
-		return await login(username, password);
+			login(username, password);
+			return !hasAuthError && onModalClose();
+		} catch (error) {
+			console.log(error);
+		}
 	};
 
 	const disableBtn = () => {
@@ -36,7 +44,7 @@ const Form: React.FC<FormProps> = ({ type }) => {
 
 	return (
 		<form className="w-80 mx-auto" onSubmit={submitHandler}>
-			<h3 className="mb-8 text-center">
+			<h3 className="mb-8 text-center text-xl uppercase tracking-wider">
 				{type === "signup" ? "Create an Account" : "Welcome back!"}
 			</h3>
 			<div className="mb-5">
@@ -76,16 +84,14 @@ const Form: React.FC<FormProps> = ({ type }) => {
 					type="password"
 					value={password}
 					onChange={(e) => {
-						error && setError(false);
+						error && setError("");
 						setPassword(e.target.value);
 					}}
 					placeholder="********"
 					className="block border h-10 w-full rounded-md px-3 text-sm outline-slate-300"
 				/>
-				{error && (
-					<small className="text-red-500">
-						Password must contain 8 characters or more!
-					</small>
+				{(error || hasAuthError) && (
+					<small className="text-red-500">{error || authError?.message}</small>
 				)}
 			</div>
 			<button
@@ -95,6 +101,39 @@ const Form: React.FC<FormProps> = ({ type }) => {
 			>
 				Submit
 			</button>
+			{type === "signup" ? (
+				<p className="text-sm mt-4 text-center">
+					Already have an account?{" "}
+					<small
+						role="button"
+						onClick={() => {
+							setEmail("");
+							setPassword("");
+							setUsername("");
+							setType("login");
+						}}
+						className="text-sm font-bold underline"
+					>
+						Login
+					</small>
+				</p>
+			) : (
+				<p className="text-sm mt-4 text-center">
+					Do not have an account?{" "}
+					<small
+						role="button"
+						onClick={() => {
+							setEmail("");
+							setPassword("");
+							setUsername("");
+							setType("signup");
+						}}
+						className="text-sm font-bold underline"
+					>
+						Signup
+					</small>
+				</p>
+			)}
 		</form>
 	);
 };
